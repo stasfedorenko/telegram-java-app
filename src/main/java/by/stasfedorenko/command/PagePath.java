3c3a73419c64bfac951969ffffc534fa8c11331c
stasfedorenko <stasfedorenko2015@mail.ru>
2022-02-19 16:01:51 +0300

    changed architecture

--- src/main/java/by/stasfedorenko/command/PagePath.java
+++ src/main/java/by/stasfedorenko/command/PagePath.java
@@ -1,9 +1,9 @@
 package by.stasfedorenko.command;
 
 public final class PagePath {
-    public static final String ERROR_404_PAGE = "/404.jsp";
-    public static final String ERROR_PAGE = "/error.jsp";
-    public static final String INDEX = "/index.jsp";
+    public static final String ERROR_404_PAGE = "${pageContext.request.contextPath}/404.jsp";
+    public static final String ERROR_PAGE = "${pageContext.request.contextPath}/error.jsp";
+    public static final String INDEX = "${pageContext.request.contextPath}/index.jsp";
 
     public static final String GO_TO_CREATE_PDF = "run?command=create_pdf";
     public static final String GO_TO_START_BOT = "run?command=start_bot";
