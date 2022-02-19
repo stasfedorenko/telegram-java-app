786bd0f0e0718cd3d36d0ee8f19f1718ea31a3c7
stasfedorenko <stasfedorenko2015@mail.ru>
2022-02-20 01:22:41 +0300

    changed architecture

--- src/main/java/by/stasfedorenko/command/PagePath.java
+++ src/main/java/by/stasfedorenko/command/PagePath.java
@@ -1,9 +1,9 @@
 package by.stasfedorenko.command;
 
 public final class PagePath{
-    public static final String ERROR_404_PAGE = "/404.jsp";
-    public static final String ERROR_PAGE = "/error.jsp";
-    public static final String INDEX_PAGE = "/index.jsp";
+    public static final String ERROR_404_PAGE = "404.jsp";
+    public static final String ERROR_PAGE = "error.jsp";
+    public static final String INDEX_PAGE = "index.jsp";
     public static final String HOME_PAGE = "/";
     public static final String DONE_PAGE = "done.jsp";
 
